f2113be6935d89ef87e135700cf58c6e728d70b3
kkvvnn <kkvvnn89@gmail.com>
2020-02-29 22:27:13 +0500

    change welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel_54
                 </div>
 
                 <div class="links">
